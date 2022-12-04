40bcb4faac96fa7085792191a6d99ab4d776450b
Iz0bEl <isobelhammonds@gmail.com>
2022-12-03 22:16:52 -0600

    Revert "Revert "Basic player movement""
    This reverts commit 4d11687f76bed2e0193eb8eeeabcc6ef32d633f0.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+
+ [Header("-----Components-----")]
+
+    [SerializeField] CharacterController controller;
+
+    [Header("-----Player Stats-----")]
+    [SerializeField] int HP;
+    [SerializeField] int jumpHeight;
+    [SerializeField] int playerSpeed;
+    [SerializeField] int gravityValue;
+    [SerializeField] int jumpsMax;
+
+
+
+    int jumpedTimes;
+    private Vector3 playerVelocity;
+    Vector3 move;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +32,30 @@ public class PlayerControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
     }
+    
+     void movement()
+    {
+        if (controller.isGrounded && playerVelocity.y < 0)
+        {
+            jumpedTimes = 0;
+            playerVelocity.y = 0f;
+        }
+
+        move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+
+        if (Input.GetButtonDown("Jump") && jumpedTimes < jumpsMax)
+        {
+            jumpedTimes++;
+            playerVelocity.y = jumpHeight;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    
 }
